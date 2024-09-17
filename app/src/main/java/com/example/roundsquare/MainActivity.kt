00a54911983a96053c0e21e1d5270b575de9a3d6
Vitaliy Zhevlakov <alyavit1@gmail.com>
2024-09-17 20:22:08 +0700

    Signed-off-by: Vitaliy Zhevlakov <alyavit1@gmail.com>

--- app/src/main/java/com/example/roundsquare/MainActivity.kt
+++ app/src/main/java/com/example/roundsquare/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.roundsquare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.pow
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val squareTextView = findViewById<TextView>(R.id.textViewSquare)
+        val radiusEditText = findViewById<TextView>(R.id.editRadiusNumber)
+        val button = findViewById<Button>(R.id.buttonRunFunc)
+
+        button.setOnClickListener(){
+            try{
+                squareTextView.text = "%.2f".format((radiusEditText.text.toString().toDouble()).pow(2) * 3.1415926535)
+            }
+            catch (e: Exception) {
+
+                var toast = Toast.makeText(applicationContext, "Error: Радиус не может быть пустым!", Toast.LENGTH_SHORT)
+                toast.show()
+
+            }
+        }
     }
+
+
 }
 
