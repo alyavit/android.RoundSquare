cfe9fac86b9a302eddf60a86534ba6bad370850d
Vitaliy Zhevlakov <alyavit1@gmail.com>
2024-10-08 19:16:54 +0700

    Лаб.раб номер 3. Вариант 8. При нажатии кнопки введенная строка выводится в обратном порядке

--- app/src/main/java/com/example/roundsquare/MainActivity.kt
+++ app/src/main/java/com/example/roundsquare/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.roundsquare
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.math.pow
+import java.util.Deque
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,31 +23,55 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener(){
             try{
-                //squareTextView.text = "%.2f".format((radiusEditText.text.toString().toDouble()).pow(2) * 3.1415926535)
-                when (radiusEditText.text.toString().toDouble()){
-                    in 0.0..1.0 -> squareTextView.text ="1"
-                    in 1.0..7.0 -> squareTextView.text =whatFirstNumber(radiusEditText.text.toString().toDouble())
-                    else -> squareTextView.text ="Значение не валидно"
-                    //TODO: давай сделай 8 вариант второй лабы. там надо функцию поудмать и туда сюда сделать мб
-                }
+                                                                                                                                                                                                                        //squareTextView.text = "%.2f".format((radiusEditText.text.toString().toDouble()).pow(2) * 3.1415926535)
+                                                                                                                                                                                                                        /*when (radiusEditText.text.toString().toDouble()){
+                                                                                                                                                                                                                            in 0.0..1.0 -> squareTextView.text ="Число в прогрессии 1"
+                                                                                                                                                                                                                            in 1.0..7.0 -> squareTextView.text = stringUpsideDown("туда сюда")
+                                                                                                                                                                                                                            else -> squareTextView.text ="Значение не валидно"
+                                                                                                                                                                                                                        }*/
+
+
+                squareTextView.text = stringUpsideDown(radiusEditText.text.toString())
             }
             catch (e: Exception) {
 
-                var toast = Toast.makeText(applicationContext, "Error!!!", Toast.LENGTH_SHORT)
+                val toast = Toast.makeText(applicationContext, "Error!!!", Toast.LENGTH_SHORT)
                 toast.show()
 
             }
         }
     }
 
-    fun whatFirstNumber (x: Double): String {
-        var i:Int = 1
-        while (x > 0 || i < 5)
-        {x - 1/i
-        i++}
-        return i.toString()
-    }
+                                                                                                                                                                                                                                                        /*fun whatFirstNumber (x: Double): String {
 
+                                                                                                                                                                                                                                                            var i:Int = 1
+                                                                                                                                                                                                                                                            var xx:Double= x;
+
+                                                                                                                                                                                                                                                            while (i < 1000)
+                                                                                                                                                                                                                                                            {
+                                                                                                                                                                                                                                                                xx -= (1.00/i);
+                                                                                                                                                                                                                                                                                                            //Log.d("Xis", "${xx} и ${i}")
+                                                                                                                                                                                                                                                                if (xx < 0){break}
+                                                                                                                                                                                                                                                                i++
+                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                            return i.toString()
+                                                                                                                                                                                                                                                        } */
+
+    fun stringUpsideDown (stringInput: String): String{
+
+        val stack = ArrayDeque<Char>()
+        var stringOutput:String = ""
+        var iter:Int = stringInput.length
+
+        while (iter > 0){
+            stack.addFirst(stringInput[stringInput.length-iter])
+            iter--
+        }
+        while (stack.size > 0){
+            stringOutput += stack.removeFirst()
+        }
+        return stringOutput
+    }
 
 }
 
