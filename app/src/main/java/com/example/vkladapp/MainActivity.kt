6f0d4d0fcf97385a94a99500997160715b1c7743
alyavit <alyavit1@gmail.com>
2024-11-05 21:20:03 +0700

    Start

--- app/src/main/java/com/example/vkladapp/MainActivity.kt
+++ app/src/main/java/com/example/vkladapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vkladapp
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
-    private val vkladViewModel: vkladViewModel by viewModels()
+    private val vkladViewModel: VkladViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +24,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val textValue = findViewById<TextView>(R.id.textView)
         val buttonRes = findViewById<Button>(R.id.button)
         val radBut3mon = findViewById<RadioButton>(R.id.radioButton3mon)
         val radBut6mon = findViewById<RadioButton>(R.id.radioButton6mon)
         val radBut12mon = findViewById<RadioButton>(R.id.radioButton12mon)
-        //test
+        //test9
+
+        buttonRes.setOnClickListener(){
+            var percent = when{
+                radBut3mon.isChecked -> 3
+                radBut6mon.isChecked -> 6
+                radBut12mon.isChecked -> 9
+                else -> 0
+            }
+
+            if (percent != 0 && (textValue.toString().toInt() > 0)) {
+                //TODO: добавить запуск нового активити с выполнением подсчета по функции
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/vkladapp/vkladViewModel.kt
+++ app/src/main/java/com/example/vkladapp/vkladViewModel.kt
@@ -2,7 +2,7 @@ package com.example.vkladapp
 
 import androidx.lifecycle.ViewModel
 
-class vkladViewModel : ViewModel() {
+class VkladViewModel : ViewModel() {
     var valueStart = 0
     var valueEnd = 0
 }
